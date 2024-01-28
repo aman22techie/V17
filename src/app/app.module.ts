2ef445bf313c0420f9f2f060fba715b2e255d021
MONOCEPT\aman <aman@monocept.com>
2024-01-28 13:24:18 +0530

    C4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './modules/authentication/login/login.component';
+import { PortalComponent } from './modules/dashboard/portal/portal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PortalComponent
   ],
   imports: [
     BrowserModule,
